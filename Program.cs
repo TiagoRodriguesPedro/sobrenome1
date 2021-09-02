80d6c0b89fbe64692922c453e8075bff7bb98e67
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-02 18:33:29 -0300

    resoluçao exercio sobrenome

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sobrenome1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome , sobrenome ;
+            Console.WriteLine("Qual seu nome");
+            nome = Console.ReadLine();
+
+           Console.WriteLine("Qual o seu sobrenome");
+           sobrenome = Console.ReadLine();
+        }
+    }
+}
